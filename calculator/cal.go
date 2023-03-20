aeb72523a3679c73ec385438e5e5e335144809c8
Thanakrit sangsuwan <191144zs456zs@gmail.com>
2023-03-20 14:38:16 +0700

    update

---
+++ calculator/cal.go
@@ -0,0 +1,5 @@
+package cal
+
+func Cal(a, b int) int {
+	return a + b
+}
---
+++ test-Go.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	cal "test-go.go/calculator"
+)
+
+func main() {
+	x := 10
+	y := 20
+	fmt.Println(cal.Cal(x, y))
+}
